92a4caa1f5c795de0c0afe742828dfe3821aa842
DACP <A31ZP@MM1A.local>
2025-09-16 14:57:35 -0600

    ajustes 3

--- Practicas/P4/reto/Ventana.java
+++ Practicas/P4/reto/Ventana.java
@@ -2,29 +2,22 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class Ventana extends JFrame {
-    JButton boton;
-    Mensajes controlador;
-    Punto punto1, punto2, punto3;
-    public Ventana(Mensajes controlador, Punto a, Punto b,Punto c) {
-        this.controlador = controlador;
-        this.punto1=a;
-        this.punto2=b;
-        this.punto3=c;
-        
-        setTitle("Comprobar si un trianguo es equilatero por la distancia de sus vertices");
-        setSize(400, 200);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
+	public Ventana(Mensajes controlador, Punto a, Punto b,Punto c) {
+		JButton boton = new JButton("Haz clic aquí para ver el resultado");
 
-        boton = new JButton("Haz clic aquí para ver el resultado");
-        boton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String mensaje = controlador.mensaje(punto1, punto2, punto3);
-                JOptionPane.showMessageDialog(null, mensaje);
-            }
-        });
+		setTitle("Comprobar si un trianguo es equilatero por la distancia de sus vertices");
+		setSize(400, 200);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
 
-        add(boton);
-    }
+		boton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String mensaje = controlador.mensaje(a, b, c);
+				JOptionPane.showMessageDialog(null, mensaje);
+			}
+		});
+
+		add(boton);
+	}
 }
