0d3f8ac4ad4728fc0c612e2517c1975ca19e127e
CasvelGit <casvel1234@gmail.com>
2025-09-17 13:56:17 -0600

    sisisisi

---
+++ Practicas/Proyecto/Proyecto.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Proyecto {
+    public static void main(String[] args) {
+        
+        Libreria Libreria1= new Libreria();
+        
+        String autor="Franz Herbert",titulo="Dune";
+        Libreria1.agregarLibro(titulo,autor);
+
+        Scanner user= new Scanner (System.in);
+        do
+        {
+            System.out.println("Elige una opción");
+            System.out.println("1.- Imprimir Libreria");
+            System.out.println("2.- Agregar Libro");
+            System.out.println("3.- Eliminar ultimo libro");
+            System.out.println("4.- Salir");
+            System.out.print(">");
+            opcion=user.nextInt();
+            switch(opcion)
+            {
+                case 1:
+                    Libreria1.printInventario();
+                break;
+
+                case 2:
+                    System.out.println("Introduce el titulo");
+                    System.out.print(">");
+                    titulo=user.next();
+                    System.out.println("Introduce el autor");
+                    System.out.print(">");
+                    autor=user.next();
+                    Libreria1.agregarLibro(titulo,autor);
+                    
+                break;
+                case 3:
+                    System.out.println("Introduce el titulo");
+                    System.out.print(">");
+                    titulo=user.next();
+                    System.out.println("Introduce el autor");
+                    System.out.print(">");
+                    autor=user.next();
+                    System.out.println("Se eliminará de la librería el libro: \n"+titulo+" ("+autor+").");
+                    Libreria1.quitarLibro(titulo,autor);
+                break;
+
+                case 4:
+                    System.out.println("Se cierra la librería");
+                    System.exit(1);
+                break;
+
+            }
+
+        }while(opcion!=4);
+
+    }
+}
+
