bc647cf290dc0d7f9493f4727056e33af2596198
CasvelGit <casvel1234@gmail.com>
2025-09-17 14:21:16 -0600

    Update Proyecto.java

--- Practicas/Proyecto/Proyecto.java
+++ Practicas/Proyecto/Proyecto.java
@@ -5,16 +5,16 @@ public class Proyecto {
         
         Libreria Libreria1= new Libreria();
         
-        String autor="Franz Herbert",titulo="Dune";
+        String autor="Franz_Herbert",titulo="Dune";
         Libreria1.agregarLibro(titulo,autor);
-
+        int opcion;
         Scanner user= new Scanner (System.in);
         do
         {
             System.out.println("Elige una opción");
             System.out.println("1.- Imprimir Libreria");
             System.out.println("2.- Agregar Libro");
-            System.out.println("3.- Eliminar ultimo libro");
+            System.out.println("3.- Eliminar un libro específico");
             System.out.println("4.- Salir");
             System.out.print(">");
             opcion=user.nextInt();
@@ -25,20 +25,20 @@ public class Proyecto {
                 break;
 
                 case 2:
-                    System.out.println("Introduce el titulo");
+                    System.out.println("Introduce el titulo (Si es más de 1 palabra separalas con guión bajo _ )");
                     System.out.print(">");
                     titulo=user.next();
-                    System.out.println("Introduce el autor");
+                    System.out.println("Introduce el autor (Si es más de 1 palabra separalas con guión bajo _ )");
                     System.out.print(">");
                     autor=user.next();
                     Libreria1.agregarLibro(titulo,autor);
                     
                 break;
                 case 3:
-                    System.out.println("Introduce el titulo");
+                    System.out.println("Introduce el titulo (Si es más de 1 palabra separalas con guión bajo _ )");
                     System.out.print(">");
                     titulo=user.next();
-                    System.out.println("Introduce el autor");
+                    System.out.println("Introduce el autor (Si es más de 1 palabra separalas con guión bajo _ )");
                     System.out.print(">");
                     autor=user.next();
                     System.out.println("Se eliminará de la librería el libro: \n"+titulo+" ("+autor+").");
