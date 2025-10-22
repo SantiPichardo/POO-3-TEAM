dfb6501d2dd5eda2118c16398fb71783c9959a5a
CasvelGit <casvel1234@gmail.com>
2025-10-22 14:18:36 -0600

    Update Empleado.java
    Cambios para que sirvan los otros codigos

--- Practicas/Proyecto2/mx/unam/fi/poo/py2/Empleado.java
+++ Practicas/Proyecto2/mx/unam/fi/poo/py2/Empleado.java
@@ -1,14 +1,28 @@
 package mx.unam.fi.poo.py2;
 
 public abstract class Empleado {
-	private final String nombre, apellidoP;
-	private final int nss;
+	private  String nombre, apellidoPaterno;
+	private int nss;
 
 	public Empleado(String n, String ap, int s) {
-		this.nombre = n; this.apellidoP = ap;
+		this.nombre = n; this.apellidoPaterno = ap;
 		this.nss = s;
 	}
+public String getNombre() {
+        return nombre;
+    }
 
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public String getNumeroSeguroSocial() {
+        return nss;
+    }
 	public abstract double ingresos();
-	public abstract String toString();
+	@Override
+    public String toString() {
+        return getNombre() + " " + getApellidoPaterno() +
+               "\nNúmero de seguro social: " + getNumeroSeguroSocial();
+    }
 }
