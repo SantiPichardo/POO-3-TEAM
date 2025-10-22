05d02a2be640051417310b4873bb7b725e9f8332
GT-8 <30545002+GT-8@users.noreply.github.com>
2025-10-22 14:26:08 -0600

    Update MainApp.java

--- Practicas/Proyecto2/mx/unam/fi/poo/py2/MainApp.java
+++ Practicas/Proyecto2/mx/unam/fi/poo/py2/MainApp.java
@@ -3,15 +3,16 @@ public class MainApp{
     public static void main (String[] args){
 
 
-        EmpleadoPorHoras empleado1 = new EmpleadoPorHoras("Juan", "Perez", "123456789", 45, 50.0);
+        EmpleadoPorHoras empleado1 = new EmpleadoPorHoras("Juan", "Perez", 1256789, 45, 50.0);
         System.out.println(empleado1.toString());
         System.out.println("Ingresos: $" + empleado1.ingresos());
 
-        EmpleadoPorHoras empleado2 = new EmpleadoPorHoras("Ana", "Lopez", "987654321", 38, 60.0);
+        EmpleadoPorHoras empleado2 = new EmpleadoPorHoras("Ana", "Lopez", 9765321, 38, 60.0);
         System.out.println(empleado2.toString());
 
 
     }
 }
+
 
 
