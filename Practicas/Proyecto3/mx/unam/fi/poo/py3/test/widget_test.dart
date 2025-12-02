31e328d652a10e64b630a398b2e78ff89584ea51
Eduardo Alvarez González <eduardo.a.g.1422@gmail.com>
2025-12-01 20:35:58 -0600

    cambio en widget_test.dart
    cambio en la definición del paquete de importación

--- Practicas/Proyecto3/mx/unam/fi/poo/py3/test/widget_test.dart
+++ Practicas/Proyecto3/mx/unam/fi/poo/py3/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:py3/main.dart';
+import 'package:batalla_pokemon/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
